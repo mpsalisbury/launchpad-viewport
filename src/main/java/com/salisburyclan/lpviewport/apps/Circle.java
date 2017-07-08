0bc22891cf7480927f678d78e445457d4a4602ab
Steven <steven@sarge.local>
2017-07-08 12:14:39 -0700

    Buil the Range Circle Generator

--- src/main/java/com/salisburyclan/lpviewport/apps/Circle.java
+++ src/main/java/com/salisburyclan/lpviewport/apps/Circle.java
@@ -1,43 +1,18 @@
 package com.salisburyclan.lpviewport.apps;
 
-import com.salisburyclan.lpviewport.api.Button2Listener;
 import com.salisburyclan.lpviewport.api.Color;
-import com.salisburyclan.lpviewport.api.LaunchpadApplication;
-import com.salisburyclan.lpviewport.api.Viewport;
 import com.salisburyclan.lpviewport.api.WriteLayer;
 import com.salisburyclan.lpviewport.geom.Point;
 
-public class Circle extends LaunchpadApplication {
+public class Circle {
 
-  private WriteLayer outputLayer;
-
-  @Override
-  public void run() {
-    getViewport(this::setupViewport);
-  }
-
-  private void setupViewport(Viewport viewport) {
-    this.outputLayer = viewport.addLayer();
-    viewport.addListener(
-        new Button2Listener() {
-          @Override
-          public void onButtonPressed(Point p) {
-            drawCircle(p, 10, Color.CYAN);
-          }
-
-          @Override
-          public void onButtonReleased(Point p) {
-            drawCircle(p, 10, Color.BLACK);
-          }
-        });
-  }
-
-  private void drawCircle(Point center, int radius, Color color) {
+  public static void drawSinglePixelCircle(
+      WriteLayer outputLayer, Point center, int radius, Color color) {
     int x = 0;
     int y = radius;
     int e = 0;
     while (y >= x) {
-      drawEight(center, x, y, color);
+      drawEight(outputLayer, center, x, y, color);
       e += 2 * x + 1;
       if (2 * e + (1 - 2 * y) <= 0) {
         x++;
@@ -49,7 +24,59 @@ public class Circle extends LaunchpadApplication {
     }
   }
 
-  private void drawEight(Point center, int x, int y, Color color) {
+  public static void drawRangeCircle(
+      WriteLayer outputLayer, Point center, int radiusIn, int radiusOut, Color color) {
+    int x = 0;
+    int yIn = radiusIn;
+    int yOut = radiusOut - 1;
+    int eIn = 0;
+    int eOut = 1 - (2 * radiusOut);
+    while (yIn >= x) {
+      drawRange(outputLayer, center, x, yIn, yOut, color);
+      //Compute new yOut and eOut
+      //Try horizontal first
+      eOut += 2 * x + 1;
+      if (eOut >= 0) {
+        //Diagonal case
+        eOut -= 2 * yOut - 1;
+        yOut--;
+      }
+      //Compute new yIn and eIn
+      //Try diagonal first
+      eIn += 2 * (x - yIn + 1);
+      if (eIn < 0) {
+        //Horizontal case
+        eIn += 2 * yIn - 1;
+      } else {
+        //Diagonal case
+        yIn--;
+      }
+      x++;
+    }
+    //After we reach the 45 degree line, we continue diagonally from the inner one to meet the outer one
+    while (yIn <= yOut) {
+      drawRange(outputLayer, center, x, yIn, yOut, color);
+      //Compute new yOut and eOut
+      //Try horizontal first
+      eOut += 2 * x + 1;
+      if (eOut >= 0) {
+        //Diagonal case
+        eOut -= 2 * yOut - 1;
+        yOut--;
+      }
+      yIn++;
+      x++;
+    }
+  }
+
+  private static void drawRange(
+      WriteLayer outputLayer, Point center, int x, int yIn, int yOut, Color color) {
+    for (int y = yIn; y <= yOut; y++) {
+      drawEight(outputLayer, center, x, y, color);
+    }
+  }
+
+  private static void drawEight(WriteLayer outputLayer, Point center, int x, int y, Color color) {
     outputLayer.setPixel(center.x() + x, center.y() + y, color);
     outputLayer.setPixel(center.x() + y, center.y() + x, color);
     outputLayer.setPixel(center.x() + y, center.y() - x, color);
